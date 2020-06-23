dc4db4986f4209037923bd5f4d3c656f3e518575
Martin Rizzo <martinrizzo@gmail.com>
2020-06-23 17:40:17 -0300

    Create a Xcode project

---
+++ extra/examples/uzo-proc/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  uzo-proc
+//
+//  Created by Martin on 23/06/2020.
+//  Copyright © 2020 Martin Rizzo. All rights reserved.
+//
+
+#include <stdio.h>
+
+#define INFLATER_IMPLEMENTATION
+#include "inflater.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
